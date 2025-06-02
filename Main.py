338b59ef61df1ef82d20ed46bd483ff1024fd929
Sean <seanpaullevesque@gmail.com>
2025-06-02 19:25:43 -0400

    working on implementation with bitwise compare

--- Main.py
+++ Main.py
@@ -4,59 +4,36 @@ import multiprocessing
 def check_guess(guess):
 
     word_list = game_class.generate_list()
-    max_remaining_words = 0
-    remaining_words_len = [guess]
-
+    word_list = game_class.add_row_numbers(word_list)
+    remaining_words_bits = []
+    print(guess)
     for word in word_list:
         remaining_words = word_list
 
-        #check if any letters are correct and in the right position
-        letters_in_same_positon = game_class.compare_common_letters_position(word, guess)
-        for position in letters_in_same_positon:
-            #remove words without letter in the correct position
-            remaining_words = game_class.letter_in_place(remaining_words, guess[position], position)
+        remaining_words = game_class.filter_list(remaining_words, guess, word[0])
 
-        #check if any letters are correct
-        common_letters = game_class.compare_common_letters(guess, word)
-        for letter in common_letters:
-            #remove words without common letters
-            remaining_words = game_class.includes(remaining_words, letter)
+        remaining_words_bits.append((sum([2 ** num[1] for num in remaining_words])))
 
-        #remove words with common letters in the wrong position
-        remaining_words = game_class.includes_wrong_place(remaining_words, guess, word)
-
-        #check if any letters are incorrect
-        diff_letters = game_class.compare_diff_letters(guess, word)
-        for letter in diff_letters:
-            #remove words that contain the incorrect letters
-            remaining_words = game_class.not_includes(remaining_words, letter)
-
-        remaining_words_len.append(len(remaining_words))
-        max_remaining_words = max(max_remaining_words, len(remaining_words))
-
-    print(str(max_remaining_words) + " " + guess)
-    return remaining_words_len
-
+    return remaining_words_bits
 
 def main():
     """Main function to execute the program."""
-    print("Program started")
 
     # init
-    word_list = game_class.generate_list()
     guess_list = game_class.generate_guess_list()
     first_guess_results = []
 
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
         first_guess_results.append(pool.map(check_guess, guess_list))
 
-    #space to allow csv header to align correctly
-    word_list.insert(0, "~")
-    first_guess_results[0].insert(0, word_list)
-    game_class.write_to_csv(first_guess_results, "first_guess_results.csv")
+    #game_class.write_to_csv(first_guess_results[0],"sixth_guess_results")
+
+
+if __name__ == "__main__":
+
+    print("Program started")
+
+    main()
 
     # Cleanup and exit
     print("Program finished")
-
-if __name__ == "__main__":
-    main()
--- game_class.py
+++ game_class.py
@@ -12,22 +12,22 @@ def generate_guess_list():
 
 #filters the word list for words that include the letter
 def includes(word_list, letter):
-    return [item for item in word_list if letter in item]
+    return [item for item in word_list if letter in item[0]]
 
 #filters the word list for words that do not include the letter
 def not_includes(word_list, letter):
-    return [item for item in word_list if letter not in item]
+    return [item for item in word_list if letter not in item[0]]
 
 #filters the word list for words that have the letter in the position
 def letter_in_place(word_list, letter, position):
-    return [item for item in word_list if letter in item[position]]
+    return [item for item in word_list if letter in item[0][position]]
 
 #filters the word list for words that do not have the common letter in the right position
 def includes_wrong_place(remaining_words, guess, word):
     for i in range(len(guess)):
         if guess[i] in word:
             if guess[i] != word[i]:
-                remaining_words = [word for word in remaining_words if word[i] != guess[i]]
+                remaining_words = [word for word in remaining_words if word[0][i] != guess[i]]
     return remaining_words
 
 def compare_common_letters_position(real_word, guess_word):
@@ -43,7 +43,48 @@ def compare_diff_letters(real_word, guess_word):
     return "".join(sorted(set(real_word) - set(guess_word)))
 
 def write_to_csv(data, filename):
-    with open(filename, mode="w", newline="") as file:
+    with open(filename+ ".csv", mode="w", newline="") as file:
         for row in data:
             for row2 in row:
                 file.write(str(row2) + "\n")
+
+def write_to_csv_in_chunks(data, filename, chunk_size):
+    for i in range(0, len(data), chunk_size):
+        chunk_filename = f"{filename}_part{i // chunk_size + 1}.csv"
+        with open(chunk_filename, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(data[i:i + chunk_size])
+
+def filter_list(remaining_words, guess, word):
+
+    #check if any letters are correct and in the right position
+    letters_in_same_positon = compare_common_letters_position(word, guess)
+    for position in letters_in_same_positon:
+        #remove words without letter in the correct position
+        remaining_words = letter_in_place(remaining_words, guess[position], position)
+
+    #check if any letters are correct
+    common_letters = compare_common_letters(guess, word)
+    for letter in common_letters:
+        #remove words without common letters
+        remaining_words = includes(remaining_words, letter)
+
+    #remove words with common letters in the wrong position
+    remaining_words = includes_wrong_place(remaining_words, guess, word)
+
+    #check if any letters are incorrect
+    diff_letters = compare_diff_letters(guess, word)
+    for letter in diff_letters:
+        #remove words that contain the incorrect letters
+        remaining_words = not_includes(remaining_words, letter)
+
+    return remaining_words
+
+def add_row_numbers(data):
+    return [[row, i] for i, row in enumerate(data, start=1)]
+
+def conflate_guesses(nested_list):
+    return [a & b for a, b in zip(nested_list[0], nested_list[1])]
+
+def sum_bits(n):
+    return sum(int(bit) for bit in bin(n)[2:])
