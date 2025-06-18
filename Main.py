42a6dfafcd9396ebb793e416c077bbf81bec7726
Sean <seanpaullevesque@gmail.com>
2025-06-17 23:14:45 -0400

    -Built a class to handle keeping track of the pickle files location in shared memory -Built capability to chunk by letter -Added some zip file stuff to minimize disk space usage

--- Main.py
+++ Main.py
@@ -3,6 +3,20 @@ import multiprocessing
 import multiprocessing.shared_memory
 import glob
 import pickle
+import time
+import statistics
+from collections import Counter
+import os
+import pandas as pd
+from pandasgui import show
+import zipfile
+import string
+
+reader = None
+
+def init_worker(meta):
+    global reader
+    reader = game_class.SharedMemoryTableReader(**meta)
 
 def check_guess(guess):
 
@@ -17,8 +31,8 @@ def check_guess(guess):
 
         remaining_words_bits.append((sum([2 ** num[1] for num in remaining_words])))
 
-        with open("guess_data/" + guess + ".pkl", "wb") as f:
-            pickle.dump(remaining_words_bits, f)
+    with open("guess_data/" + guess + ".pkl", "wb") as f:
+        pickle.dump(remaining_words_bits, f)
 
     return
 
@@ -26,85 +40,189 @@ def data_collect():
 
     # init
     guess_list = game_class.generate_guess_list()
-    first_guess_results = []
 
+    check_guess(guess_list)
 
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-        first_guess_results.append(pool.map(check_guess, guess_list))
-
+        pool.map(check_guess, guess_list)
 
 
 def data_crunch():
 
-    # init
+    #init shared memory writer
+    writer = game_class.SharedMemoryTableWriter()
+
+    #filter the guess list to remove words with double letters
     guess_list = game_class.generate_guess_list()
+    guess_list = [word for word in guess_list if len(set(word)) == len(word)]
 
-    # Get all pickle file paths
-    pkl_files = glob.glob("guess_data/*.pkl")  # Adjust directory path
+    #load all of the pickle files into memory serialized using the SharedMemoryTableWriter class
+    for word in guess_list:
+        writer.add(word, pickle.load(open("C:/wordle/first_guess_data/" + word + ".pkl", "rb")))
 
-    # Use a generator to stream serialization without keeping all objects in memory
-    serialized_data = b"".join(pickle.dumps(pickle.load(open(file, "rb"))) for file in pkl_files)
+    metadata = writer.finalize()
 
-    #allocate shared memory
-    shm = multiprocessing.shared_memory.SharedMemory(create=True, size=len(serialized_data))
+    #chunk one letter at a time to save on disk space
+    #using slices so that I can continue from where I last left off
+    for letter in string.ascii_lowercase[string.ascii_lowercase.index("g"):]:
+        #generate the list of words to check and then filter out words with double and common letters
+        guess_list = game_class.generate_guess_list()
+        guess_list = [word for word in guess_list if len(set(word)) == len(word)]
+        guess_list = [word for word in guess_list if word.lower().startswith(letter)]
 
-    #write serialized data to shared memory
-    shm.buf[: len(serialized_data)] = serialized_data
+        #pack arguments as tuples (metadata, guess_list_item)
+        args_list = [(metadata, guess) for guess in guess_list]
 
-    #pack arguments as tuples (shared_memory_name, guess_list_item)
-    args_list = [(shm.name, guess) for guess in guess_list[50:100]]
+        with multiprocessing.Pool(initializer=init_worker, initargs=(metadata,)) as pool:
+            pool.map(process_combinations, args_list)
 
-    #free up RAM
-    #serialized_data = None
+        #file generation complete, put everything into a zip file
+        source_folder = "C:/wordle/staging/second_guess_data/"
+        output_folder = "C:/wordle/second_guess_data/"
+        zip_filename = os.path.join(output_folder, f"{letter}.zip")
 
-    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-        pool.map(process_combinations, args_list)
+        files = glob.glob(os.path.join(source_folder, letter + "*"))
 
+        with zipfile.ZipFile(zip_filename, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
+            for filepath in files:
+                arcname = os.path.relpath(filepath, start=source_folder)
+                zipf.write(filepath, arcname=arcname)
+                os.remove(filepath)
 
+#this takes in a guess and then conflates it with every other guess
+#it generates a pickle file into C:/wordle/staging
+#then it pulls all of the pickle files together into a zip file to save disk space
 def process_combinations(args):
 
     #unpack arguments
-    shm_name, guess = args
+    metadata, guess1 = args
+    print(guess1)
 
-    print(guess)
-    existing_shm = multiprocessing.shared_memory.SharedMemory(name=shm_name)
+    #point to shared memory
+    guess1_data = reader.get(guess1)
 
-    #generate a list to acquire the index
-    guess_dict = game_class.generate_guess_dict_sizes()
-    #generate a dictionary to acquire the size of each data
-    guess_list = game_class.generate_guess_list()
+    #load the guess2 list and filter down to remove common and double letters
+    guess2_list = game_class.generate_guess_list()
+    guess2_list = [word for word in guess2_list if not any(letter in word for letter in guess1)]
+    guess2_list = [word for word in guess2_list if len(set(word)) == len(word)]
 
-    guess_index = guess_list.index(guess)
-    guess_start = guess_dict[guess_list[guess_index - 1]] if guess_index > 0 else 0
-    guess_end = guess_dict[guess]
-    guess1_data = pickle.loads(memoryview(existing_shm.buf[guess_start: guess_end]))
-
-    for i, file in enumerate(guess_list):
-        guess_start = guess_dict[guess_list[i - 1]] if i > 0 else 0
-        guess_end = guess_dict[file]
-
-        guess2_data = pickle.loads(memoryview(existing_shm.buf[guess_start: guess_end]))
+    for guess2 in guess2_list:
+        time.sleep(0.01)
+        guess2_data = reader.get(guess2)
 
         result = [a & b for a, b in zip(guess1_data, guess2_data)]
 
-        #compute sum of binary digits for each number
-        max_value = max(map(lambda num: bin(num).count("1"), result))
+        output_filename = os.path.join("C:/wordle/staging/second_guess_data", f"{guess1} {guess2}.pkl")
+        with open(output_filename, "wb") as f:
+            pickle.dump(result, f)
 
-        if max_value <30:
 
-            output_filename = f"second_guess_data/{guess} {file}.pkl"
-            with open(output_filename, "wb") as f:
-                pickle.dump(result, f)
+def data_analyze():
 
-    existing_shm.close()
+    # Get all pickle file paths
+    args_list = glob.glob("C:/wordle/second_guess_data/*.pkl")
+    print("guess 1st 2nd 3rd 4th 1count 2count 3count 4count")
 
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        pool.map(check_data_single, args_list)
+
+#this opens a single pickle file and generates statistics and prints them to console
+def check_data_single(guess):
+
+    combo_guess = pickle.load(open(guess, "rb"))
+
+    binary_numbers = [bin(n).count("1") for n in combo_guess]
+    ones_count = list(map(lambda n: bin(n).count("1"), combo_guess))
+    ones_count = Counter(ones_count)[1]
+    min_count = min(binary_numbers)
+    max_count = max(binary_numbers)
+    quartiles = statistics.quantiles(binary_numbers, n=4)
+    print(f"{os.path.splitext(os.path.basename(guess))[0]} {min_count} {quartiles[0]} {quartiles[1]} {quartiles[2]} {max_count} {ones_count}\n")
+
+#this takes a single pickle file and conflates it with every other pickle file and prints statistics to console
+def check_data_double(combo1_guess):
+
+    combo1_data = pickle.load(open(combo1_guess, "rb"))
+    # Get all pickle file paths
+    combo2_list = glob.glob("second_guess_data/*.pkl")
+
+    filtered_words = [os.path.splitext(os.path.basename(word))[0] for word in combo2_list if not any(letter in word for letter in combo1_guess)]
+
+    for combo2_guess in filtered_words:
+        combo2_data = pickle.load(open(combo2_guess, "rb"))
+        result = [a & b for a, b in zip(combo1_data, combo2_data)]
+        combo1_filename = os.path.splitext(os.path.basename(combo1_guess))[0]
+        combo2_filename = os.path.splitext(os.path.basename(combo2_guess))[0]
+
+        binary_numbers = [bin(n).count("1") for n in result]
+        ones_count = list(map(lambda n: bin(n).count("1"), result))
+        ones_count = Counter(ones_count)[1]
+        min_count = min(binary_numbers)
+        max_count = max(binary_numbers)
+        quartiles = statistics.quantiles(binary_numbers, n=4)
+        print(f"{combo1_filename} {combo2_filename} {min_count} {quartiles[0]} {quartiles[1]} {quartiles[2]} {max_count} {ones_count}\n")
+
+        output_filename = f"fourth_guess_data/{combo1_filename} {combo2_filename}.pkl"
+        with open(output_filename, "wb") as f:
+            pickle.dump(result, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def check_data2(args_list):
+
+    guess1_data, guess2 = args_list
+    print(guess2)
+    third_guess = "dampy"
+    guess3_data = pickle.load(open("guess_data/" + third_guess + ".pkl", "rb"))
+    fourth_guess = glob.glob("guess_data/*.pkl")  # Adjust directory path
+    for guess4 in fourth_guess:
+        time.sleep(0.02)
+        guess4_data = pickle.load(open(guess4, "rb"))
+
+        guess2_data = pickle.load(open("guess_data/" + guess2 + ".pkl", "rb"))
+        result = [a & b for a, b in zip(guess1_data, guess2_data)]
+        result = [a & b for a, b in zip(result, guess3_data)]
+        result = [a & b for a, b in zip(result, guess4_data)]
+        # compute sum of binary digits for each number
+        max_combo = max(map(lambda num: bin(num).count("1"), result))
+        ones_count = list(map(lambda n: bin(n).count("1"), result))
+        count = Counter(ones_count)[1]
+        if count > 2200:
+            print("count " + guess2 + " " + guess4 + " " + " " + str(count))
+        # binary_numbers = [bin(n).count("1") for n in result]
+        # quartiles = statistics.quantiles(binary_numbers, n=4)
+        # third_quartile = quartiles[2]  # Q3 (75th percentile)
+        #
+        # if third_quartile < 2:
+        #     print(f"Third quartile: {guess4} {third_quartile} ")
+
+        if max_combo <3:
+        #     print(file.split("\\")[-1] + " " + str(max_combo))
+            print("Max " + guess2 + " " + guess4 + " " + " " + str(max_combo))
 
 if __name__ == "__main__":
 
     print("Program started")
+    # dtype_map = {0: str, 1: str, 2: int, 3: int, 4: int, 5: int, 6: int, 7: int}
+    # df = pd.read_csv(r"C:\Users\Sean\Downloads\test - Copy.csv",
+    #                  sep=r"\s+",  # Treat consecutive whitespace as delimiter
+    #                  skip_blank_lines=True,
+    #                  dtype={"guess1": str, "guess2": str, "min": int, "1stq": int, "median": int, "3rdq": int, "max": int, "count": int})  # Skip empty rows automatically
+    #
+    # gui = show(df)
 
+    # letter = "c"
+    # source_folder = "second_guess_data/"
+    # zip_filename = letter + ".zip"
+    #
+    # files = glob.glob(os.path.join(source_folder, letter + "*"))  # Only top-level files starting with 'a'
+    #
+    # with zipfile.ZipFile(zip_filename, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
+    #     for filepath in files:
+    #         print(filepath)
+    #         arcname = os.path.relpath(filepath, start=source_folder)
+    #         zipf.write(filepath, arcname=arcname)
     #data_collect()
     data_crunch()
+    #data_analyze()
 
     # Cleanup and exit
     print("Program finished")
--- game_class.py
+++ game_class.py
@@ -1,6 +1,62 @@
-import csv
-import itertools
+import pickle
+from multiprocessing import shared_memory
 
+#this class handles writing pickle files into serialized memory
+#it keeps track of the start and end of each pickle file so that I can point to them later
+class SharedMemoryTableWriter:
+    def __init__(self):
+        self.offsets = {}  # filename → (start, end)
+        self.current_offset = 0
+        self.data_chunks = []
+
+    def add(self, filename, obj):
+        chunk = pickle.dumps(obj)
+        start = self.current_offset
+        end = start + len(chunk)
+        self.offsets[filename] = (start, end)
+        self.data_chunks.append(chunk)
+        self.current_offset = end
+
+    def finalize(self):
+        #serialize full data block
+        data_blob = b"".join(self.data_chunks)
+        self.data_shm = shared_memory.SharedMemory(create=True, size=len(data_blob))
+        self.data_shm.buf[:len(data_blob)] = data_blob
+
+        #serialize offset table
+        offset_blob = pickle.dumps(self.offsets)
+        self.offset_shm = shared_memory.SharedMemory(create=True, size=len(offset_blob))
+        self.offset_shm.buf[:len(offset_blob)] = offset_blob
+
+        return {
+            "data_name": self.data_shm.name,
+            "data_size": len(data_blob),
+            "offsets_name": self.offset_shm.name,
+            "offsets_size": len(offset_blob)
+        }
+
+    def cleanup(self):
+        self.data_shm.close()
+        self.data_shm.unlink()
+        self.offset_shm.close()
+        self.offset_shm.unlink()
+
+class SharedMemoryTableReader:
+    def __init__(self, data_name, data_size, offsets_name, offsets_size):
+        self.data_shm = shared_memory.SharedMemory(name=data_name)
+        self.offsets_shm = shared_memory.SharedMemory(name=offsets_name)
+        #create a memoryview of the data buffer once
+        self.data_view = memoryview(self.data_shm.buf)
+        self.offsets = pickle.loads(bytes(self.offsets_shm.buf[:offsets_size]))
+
+    def get(self, filename):
+        start, end = self.offsets[filename]
+        return pickle.loads(bytes(self.data_view[start:end]))
+
+    def cleanup(self):
+        self.data_view.release()
+        self.data_shm.close()
+        self.offsets_shm.close()
 
 def generate_list():
     with open("wordle_words", "r") as file:
@@ -8,18 +64,6 @@ def generate_list():
 
 def generate_guess_list():
     with open("dict", "r") as file:
-        return [line.strip() for line in file]
-
-def generate_guess_list_sizes():
-    with open("dict_sizes", "r") as file:
-        return [line.strip() for line in file]
-
-def generate_guess_dict_sizes():
-    with open("dict", "r") as f1, open("dict_sizes", "r") as f2:
-        return {line1.strip(): int(line2.strip()) for line1, line2 in zip(f1, f2)}
-
-def generate_second_guess_list():
-    with open("best_second_guesses", "r") as file:
         return [line.strip() for line in file]
 
 #filters the word list for words that include the letter
@@ -60,12 +104,6 @@ def write_to_csv(data, filename):
             for row2 in row:
                 file.write(str(row2) + "\n")
 
-def write_to_csv_in_chunks(data, filename, chunk_size):
-    for i in range(0, len(data), chunk_size):
-        chunk_filename = f"{filename}_part{i // chunk_size + 1}.csv"
-        with open(chunk_filename, mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerows(data[i:i + chunk_size])
 
 def filter_list(remaining_words, guess, word):
 
@@ -100,8 +138,3 @@ def conflate_guesses(list1, list2):
 
 def sum_bits(n):
     return sum(int(bit) for bit in bin(n)[2:])
-
-def chunk_combinations(iterable, size):
-    iterator = iter(iterable)
-    while chunk := list(itertools.islice(iterator, size)):
-        yield chunk
